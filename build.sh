aa8b9274e5c3230072a7e001008bf47c1192e537
David Gasquez <davidgasquez@gmail.com>
2017-08-22 12:19:03 +0200

    Install libraries to specific locations

--- build.sh
+++ build.sh
@@ -9,16 +9,15 @@ if [ ! -d venv ]; then
     python3 -m venv venv
 fi
 
-deactivate || true
 source venv/bin/activate
-pip install --user -r requirements.txt
+pip install -r requirements.txt -t lib
 
 # Zip the lambda
 zip -9 lambda.zip main.py
 
 # Zip the dependencies
-cd venv/lib/python3.6/site-packages/
-zip -r9 ../../../../lambda.zip .
+cd lib
+zip -r9 ../lambda.zip .
 cd -
 
 # Exit the virtual environment
